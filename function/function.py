c0ce2ab982fff6273696953f808c27aa40879918
Maksim <maksimum232@gmail.com>
2022-07-22 21:19:38 +0200

    Added documentation

--- function/function.py
+++ function/function.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import gunicorn
 
 import subprocess as sp
 import re
@@ -20,7 +19,7 @@ resource_map = {
     "Apigee": ["apigee"], "Big Data": ["bigquery", "composer", "dataproc"], "App Engine": ["gae"],
     "CI/CD": ["gcb"], "Integration": ["gcb"], "Compute Engine": ["gce"], "Cloud Functions": ["gcf"],
     "Storage": ["gcs"], "Kubernetes Engine": ["gke"], "IAM": ["iam"], "Ass": ["tpu"], "Networking": ["vpc"],
-    "All": "All"
+    "All": ["All"]
 }
 
 # Auth key
@@ -36,6 +35,19 @@ jira = JIRA(basic_auth=("maksim.turtsevich@devoteam.com", os.getenv("jira_token"
 
 
 def generate_rule_with_description(raw_rule_with_description):
+    """
+    Function that processes the gcpdiag rule string in a human readable
+    format, corrects prefixes if needed and outputs them for further
+    processing and filtering
+
+    Parameters:
+    raw_rule_with_description(str): raw gcpdiag rule with its description
+
+    Returns:
+    final_rule_with_description (str): gcpdiag rule string in a human readable format
+    final_prefix (str): prefix for further filtering and validation
+
+    """
     rule = re.search(pattern, raw_rule_with_description).group()
     rule_prefix = rule.split("/")[0]
 
@@ -51,6 +63,21 @@ def generate_rule_with_description(raw_rule_with_description):
 
 
 def extract_mentioned_resources(lst_of_resources):
+    """
+    Function extracts the prefixes of the affected resources
+    using the "resource_map", adds them in a set (in order to
+    avoid the duplicates) and outputs it for filtering
+
+    Parameters:
+        lst_of_resources(list): List of types of resources
+                                requested to be inspected
+                                (e.g Compute Engine, GKE)
+
+    Returns:
+        all_prefixes(str): set of the prefixes that can be
+                           used to match the resources affected
+                           or requested for inspection
+    """
     all_prefixes = []
 
     for field in lst_of_resources:
@@ -63,6 +90,18 @@ def extract_mentioned_resources(lst_of_resources):
 
 
 def validate_the_resource(starting_rule, data, prefix):
+    """
+    Function that validates the rule inputted (if it should
+    be included in the final outputted string) and outputs
+    True or False boolean value
+
+    Parameters:
+        data(dict): List of logs grouped by rules
+        prefix(str): Payload of the Webhook
+
+    Returns:
+        bool
+    """
     lst_of_resources = data["issue"]["fields"]["resource"]
     all_prefixes = extract_mentioned_resources(lst_of_resources)
     if prefix not in all_prefixes and lst_of_resources[0] != "All":  # Testing thing will be changed later
@@ -71,18 +110,43 @@ def validate_the_resource(starting_rule, data, prefix):
     return True
 
 
-def generate_final_string(logs_divided_by_rules, data):
+def generate_final_string(logs_divided_by_rules, data) -> str:
+    """
+    Function that generates a final string from the logs
+    that are divided by gcpdiag rules
+
+    Parameters:
+        logs_divided_by_rules(list): List of logs grouped by rules
+        data(dict): Payload of the Webhook
+
+    Returns:
+        final_string(str): Processed gcpdiag logs
+    """
+
     final_string = ""
     rules_count = 1
+
+    # Iterating through the list of rules and their respective data
     for rule_lst in logs_divided_by_rules:
+
+        # If length of the rule's list is 1 => it means that there is no data associated
+        # with a particular rule, as a result it will be passed
         if len(rule_lst) == 1:  # Last item without filters contains two elements in the list [rule, ""], fix it
             continue
 
+        # Calling method that processes the string with the rule in a human readable format
+        # and to extract the prefix for futher filtering
         starting_rule, prefix_for_validation = generate_rule_with_description(rule_lst[0])
+
+        # Calling a method in order to validate whether a prefix of a specific rule that
+        # concerns a specific resource (e.g gke, coe, gcf) need to be outputted
         validation = validate_the_resource(starting_rule, data, prefix_for_validation)
+
+        # If validation equals "False" the record will be passed
         if not validation:
             continue
 
+        # Building the final string
         final_string += str(rules_count) + ")" + " " + starting_rule + "\n"
         rules_count += 1
 
@@ -109,25 +173,43 @@ def generate_final_string(logs_divided_by_rules, data):
 
 
 def processing(logs):
+    """
+    Function that groups the logs by the rules  (i.e gke/ERR/2022_001, iam/SEC/2021_001)
+    and stores all the related data (documentation, resources affected) in the same list
+    with them for further processing
+
+    Parameters:
+        logs (list): Logs divided by rows
+
+    Returns:
+        logs_divided_by_rules (str): Logs that are grouped by rules
+    """
     # Initial Processing
     new_logs = [log.strip() for log in logs]
 
     logs_divided_by_rules = []
+
+    # Iterates through the list of rows
     for i in range(len(new_logs)):
         rule_match = re.search(pattern, new_logs[i])
+
+        # If rule matches the regex pattern for "gcpdiag" rules and doesn't
+        # have an "http" in it. It will be considered as a "host of the group"
         if rule_match and i != len(new_logs) - 1 and "http" not in new_logs[i]:
             next_string = re.search(pattern, new_logs[i + 1])
             unmatched_string_index = i + 1
 
+            # Iterating through the values after the "matched" string
+            # until the new matched string (group) will be found
             while not next_string and unmatched_string_index != len(new_logs) - 1:
                 unmatched_string_index += 1
                 next_string = re.search(pattern, new_logs[unmatched_string_index])
 
-                # URL with rule matches the REGEX pattern as well, in order to skip it we need put next_string to False
-                # in order to not exit the loop
                 if next_string and "http" in new_logs[unmatched_string_index]:
                     next_string = False
 
+            # Adding slice of values from the first "matched" string (rule)
+            # to the second "matched" string (rule) in the list of lists
             logs_divided_by_rules.append(new_logs[i:unmatched_string_index])
         else:
             continue
@@ -136,6 +218,16 @@ def processing(logs):
 
 
 def logs_processing_driver(gcpdiag_logs, data):
+    """
+    Function that executes all the data processing methods
+
+    Parameters:
+        gcpdiag_logs (str): Logs outputted for "gcpdiag"
+        data (dict): payload of the Webhook
+
+    Returns:
+        final_string (str) - processed logs from "gcpdiag"
+    """
     lst_of_logs = gcpdiag_logs.splitlines()
 
     logs_divided_by_rules = processing(lst_of_logs)
@@ -145,30 +237,65 @@ def logs_processing_driver(gcpdiag_logs, data):
 
 
 def execute_gcpdiag(project_name: str):
-    print("executing command")
-    command = f"sudo ./gcpdiag lint --project {project_name} --hide-ok --auth-key={authentication_key}"
-    # command = f"./gcpdiag lint --project {project_name} --hide-ok"
-    logs = sp.getoutput(command)
+    """
+    Function executes the "gcpdiag" command and collects
+    the outputs of it
 
-    print("Output of the command ", logs)
+    Parameters:
+        project_name (str): Name of the project to extract
+                        logs from
+
+    Returns:
+        logs (str) - logs outputted from "gcpdiag"
+    """
+
+    # Command for running "gcpdiag"
+    # command = f"sudo ./gcpdiag lint --project {project_name} --hide-ok --auth-key={authentication_key}"
+    command = f"./gcpdiag lint --project {project_name} --hide-ok"
+
+    # Storing the output of the command
+    logs = sp.getoutput(command)
 
     return logs
 
 
 def submit_to_jira(name_of_the_ticket, final_string):
+    """
+    Function submits the internal comment to the Jira ticket
+    by using the name of the ticket (name_of_the_ticket) and
+    comment itself (final_string)
+
+    Parameters:
+        name_of_the_ticket (str): Name of the ticket
+        final_string (str): String to put in the comment
+
+    """
+
     comment = jira.add_comment(name_of_the_ticket, final_string, visibility={'key': 'sd.public.comment'},
                                is_internal=True)
 
 
 @app.route("/", methods=['POST', 'GET'])
 def main():
+    """
+    Main function that listens to the incoming requests
+    and handles the Webhooks from Jira, i.e receives POST
+    requests and extracts the needed data from them.
+
+    Returns:
+        final_string (str) - final string that's being returned
+        after all the processing performed
+    """
+
+    # If request method is GET, return 'Hello, World!' for testing purposes
     if request.method == "GET":
         return "Hello World!"
 
-    print("request: POST")
+    # Extracting the data from the request
     data = request.json
     project_name = data["issue"]["fields"]["GCP Project ID"]
 
+    # Running gcpdiag command and running logs_processing_driver method
     gcpdiag_logs = execute_gcpdiag(project_name)
     final_string = logs_processing_driver(gcpdiag_logs, data)
 
